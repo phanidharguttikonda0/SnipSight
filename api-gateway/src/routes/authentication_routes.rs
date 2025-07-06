5175e1e031463b5ea7d25899755788028bd61a41
phanidharguttikonda0 <phanidharguttikonda0@gmail.com>
2025-07-06 16:14:05 +0530

    added authentication routes

--- api-gateway/src/routes/authentication_routes.rs
+++ api-gateway/src/routes/authentication_routes.rs
@@ -1,5 +1,15 @@
 use axum::Router;
+use axum::routing::post;
 
 pub fn authentication_routes() -> Router {
     Router::new()
+        .route("/sign-in", post(|| async{
+            tracing::info!("handling sign-in request") ;
+        }))
+        .route("/sign-up", post(|| async{
+            tracing::info!("handling sign-up request") ;
+        }))
+        .route("/forgot-credentials", post(|| async{
+            tracing::info!("handling forgot request") ;
+        })) 
 }
