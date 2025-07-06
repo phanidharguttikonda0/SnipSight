730593d55484772d75dbdb27207aba8c00e187de
phanidharguttikonda0 <phanidharguttikonda0@gmail.com>
2025-07-06 21:39:03 +0530

    added docker file

--- authentication_app/src/main.rs
+++ authentication_app/src/main.rs
@@ -1,7 +1,7 @@
 mod middlewares;
 mod handlers;
 
-use axum::{Json, Router};
+use axum::{Router};
 use axum::routing::{post, get};
 use tracing_subscriber;
 use crate::handlers::{sign_in_handler, sign_up_handler};
@@ -13,11 +13,11 @@ async fn main() {
     let app = Router::new()
         .route("/", get(|| async {
             "Just a Test whether Server working properly or not"
-        }))   
+        }))
     .route("/sign-in/{username}/{password}", post(sign_in_handler))
     .route("/sign-up/{username}/{password}/{mail_id}/{mobile}", post(sign_up_handler));
 
-    let tcp_listener = tokio::net::TcpListener::bind("0.0.0.0:9091").await.unwrap();
+    let tcp_listener = tokio::net::TcpListener::bind("0.0.0.0:9090").await.unwrap();
     tracing::info!("Listening on {}", tcp_listener.local_addr().unwrap());
     axum::serve(tcp_listener, app).await.unwrap()
 
