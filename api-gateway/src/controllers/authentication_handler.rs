0e4282e6c1763f56fb3a85005b0b5b1dab2638d8
phanidharguttikonda0 <phanidharguttikonda0@gmail.com>
2025-07-11 18:27:27 +0530

    updated the local communication url between services

--- api-gateway/src/controllers/authentication_handler.rs
+++ api-gateway/src/controllers/authentication_handler.rs
@@ -8,7 +8,7 @@ use crate::models::authentication_models::{Login, Register};
 use crate::middlewares::response_creator::response_creator;
 pub async fn sign_in(Form(login) :Form<Login>) -> Result<impl IntoResponse, impl IntoResponse> {
     let client = Client::new() ;
-    match client.get(format!("http://authentication-service:9090/sign-in/{}/{}",login.username,login.password)).send().await {
+    match client.get(format!("http://authentication-container:9090/sign-in/{}/{}",login.username,login.password)).send().await {
         Ok(response) => {
             tracing::info!("Got the Response from the authentication server") ;
             tracing::info!("Now going to rebuild the response compatible with the AXUM IntoResponse") ;
@@ -29,7 +29,7 @@ pub async fn sign_in(Form(login) :Form<Login>) -> Result<impl IntoResponse, impl
 
 pub async fn sign_up(Form(register) :Form<Register>) -> impl IntoResponse{
     let client = Client::new() ;
-    match client.get(format!("http://authentication-service:9090/sign-up/{}/{}/{}/{}/{}",
+    match client.get(format!("http://authentication-container:9090/sign-up/{}/{}/{}/{}/{}",
     register.username, register.password, register.mail_id, register.mobile, register.country_id)).send().await{
         Ok(response) => {
             tracing::info!("Got the Response from the authentication server") ;
