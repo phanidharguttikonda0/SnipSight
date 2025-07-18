8b923d51fafb8a712c9b44dbd538192386153bc3
phanidharguttikonda0 <phanidharguttikonda0@gmail.com>
2025-07-18 18:14:04 +0530

    written logic to get rds url from aws ssm parameter

--- authentication_app/src/main.rs
+++ authentication_app/src/main.rs
@@ -1,6 +1,10 @@
 mod middlewares;
 mod handlers;
 mod state;
+
+
+use aws_config::meta::region::RegionProviderChain;
+use aws_sdk_ssm::Client;
 use sqlx::{PgPool, Pool, Postgres};
 use tracing_subscriber;
 use authentication_app::create_app;
@@ -31,7 +35,31 @@ async fn main() {
 async fn get_rds_url() -> String {
     // here we need to get the urls from the aws secret manager or what ever we use
     tracing::info!("going to get rds url");
-    String::from("postgres://postgres:Phani9090Kl@snipshare.cbigqkgesirb.ap-south-1.rds.amazonaws.com:5432/authentication_app")
+
+    let region_provider = RegionProviderChain::default_provider().or_else("ap-south-1");
+    let config = aws_config::from_env().region(region_provider).load().await;
+
+    // Create the SSM client
+    let client = Client::new(&config);
+
+    // Define your parameter name
+    let param_name = "/snipsight/db/rds";
+
+    // Fetch the parameter with decryption enabled
+    let result = client
+        .get_parameter()
+        .name(param_name)
+        .with_decryption(true)
+        .send()
+        .await.unwrap();
+
+    // Extract and print the value
+    if let Some(value) = result.parameter().and_then(|p| p.value()) {
+        format!("{}{}",value, "authentication_app")
+    } else {
+        String::from("")
+    }
+
     // String::from("postgres://postgres:phani@localhost:5432/authentication_app") // -- for local testing
 }
 
