354de1ca92bad3a4f81c22d17d1b9360a45563e4
phanidharguttikonda0 <phanidharguttikonda0@gmail.com>
2025-07-23 18:54:51 +0530

    updated ui

--- url-shortener-app/app/dashboard/page.tsx
+++ url-shortener-app/app/dashboard/page.tsx
@@ -456,7 +456,7 @@ export default function DashboardPage() {
                                     </div>
                                     <div>
                                       <div
-                                          className="truncate font-medium text-gray-900 max-w-[36ch]"
+                                          className="truncate font-medium text-gray-900 max-w-[27ch]"
                                           title={url.original_url}
                                       >
                                         {url.original_url}
